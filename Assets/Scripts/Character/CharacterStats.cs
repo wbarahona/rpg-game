ada99d8aa584f736df9d931e61dc111af2d4ce4c
Willmer Barahona <wbarahona@live.com>
2023-05-17 03:00:59 +0200

    updated the player prefab name, the circle collider 2d to be smaller so it can be slightly above other solid objects sprite, updated the sorting layer to be player layer duh! added animations for up, down, left and right, taking a hit, dying, attacking, player attack when on battle scene and enemy attack when in battle as well, created blend trees, and an character animation script that handles all vars for the animator

--- Assets/Scripts/Character/CharacterStats.cs
+++ Assets/Scripts/Character/CharacterStats.cs
@@ -24,6 +24,7 @@ public class CharacterStats : MonoBehaviour
   {
     player,
     npc,
+    enemy,
     boss,
     merchant,
     questGiver,
--- Assets/Scripts/NPC/AIMovement.cs
+++ Assets/Scripts/NPC/AIMovement.cs
@@ -2,57 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIMovement : MonoBehaviour {
-    public float speed = 6.5f;
-    public float avoidDistance = 1f;
-    public float moveDelay = 3f; // Delay between movements in seconds
-    public LayerMask obstacleLayer;
+public class AIMovement : MonoBehaviour
+{
+  public float speed = 3.5f;
+  public float avoidDistance = 1f;
+  public float moveDelay = 3f; // Delay between movements in seconds
+  public LayerMask obstacleLayer;
+  private Rigidbody2D rb;
+  private CharacterAnimations characterAnimations;
+  Vector2 direction;
 
-    private Rigidbody2D rb;
+  void Start()
+  {
+    characterAnimations = GetComponent<CharacterAnimations>();
+    rb = GetComponent<Rigidbody2D>();
+    StartCoroutine(MoveRandomly());
+  }
 
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(MoveRandomly());
+  IEnumerator MoveRandomly()
+  {
+    while (true)
+    {
+      characterAnimations.x = 0;
+      characterAnimations.y = 0;
+
+      yield return new WaitForSeconds(moveDelay);
+      direction = GetRandomDirection();
+      // TODO: why is it that the character is facing the wrong way when moving left or down? I had to add the * -1 to fix it
+      characterAnimations.x = direction.x * -1;
+      characterAnimations.y = direction.y * -1;
+      rb.velocity = direction * speed;
+
+      if (CheckForObstacles())
+      {
+        rb.velocity = -direction * speed;
+      }
+
+      yield return new WaitForSeconds(.62f);
     }
+  }
 
-    IEnumerator MoveRandomly() {
-        while (true)
-        {
-            Vector2 direction = GetRandomDirection();
-            rb.velocity = direction * speed;
-
-            if (CheckForObstacles()) {
-                rb.velocity = -direction * speed;
-            }
-
-            yield return new WaitForSeconds(moveDelay);
-        }
+  private Vector2 GetRandomDirection()
+  {
+    int randomInt = Random.Range(0, 4);
+    switch (randomInt)
+    {
+      case 0:
+        return Vector2.up;
+      case 1:
+        return Vector2.down;
+      case 2:
+        return Vector2.left;
+      default:
+        return Vector2.right;
     }
+  }
 
-    private Vector2 GetRandomDirection() {
-        int randomInt = Random.Range(0, 4);
-        switch (randomInt) {
-            case 0:
-                return Vector2.up;
-            case 1:
-                return Vector2.down;
-            case 2:
-                return Vector2.left;
-            default:
-                return Vector2.right;
-        }
+  private bool CheckForObstacles()
+  {
+    RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up, avoidDistance, obstacleLayer);
+    RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down, avoidDistance, obstacleLayer);
+    RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, avoidDistance, obstacleLayer);
+    RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, avoidDistance, obstacleLayer);
+
+    if (hitUp.collider != null || hitDown.collider != null || hitLeft.collider != null || hitRight.collider != null)
+    {
+      return true;
     }
-
-    private bool CheckForObstacles() {
-        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up, avoidDistance, obstacleLayer);
-        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down, avoidDistance, obstacleLayer);
-        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, avoidDistance, obstacleLayer);
-        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, avoidDistance, obstacleLayer);
-
-        if (hitUp.collider != null || hitDown.collider != null || hitLeft.collider != null || hitRight.collider != null) {
-            return true;
-        } else {
-            return false;
-        }
+    else
+    {
+      return false;
     }
+  }
 }
